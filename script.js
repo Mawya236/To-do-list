8a569091c3516103ebdf0b0a60833660238bd9ad
Mawya236 <mayaellaithy99@gmail.com>
2024-08-25 02:18:44 +0300

    add

--- script.js
+++ script.js
@@ -1,67 +1,83 @@
-let addTask = document.getElementById('add-task');
-let AddBtn = document.getElementById('AddBtn');
-let add;
-let mood = 'ADD';
-let x;
-let display = 0;
-if(localStorage.Task != null){
-    add = JSON.parse(localStorage.Task);
-}
-else{
-    add = [];
-}
-AddBtn.onclick = function(){
-    let newTask = addTask.value;
-    if(mood === 'ADD'){
-        add.push(newTask);
-        addTask.value = '';
+let AddTask = document.getElementById("AddTask");
+let AddBtn = document.getElementById("AddBtn");
+let NewTask = document.getElementById("NewTask");
+let editebtn = document.getElementById("editebtn");
+let add = [];
+let Btnremove;
+//ADD TASK
+AddBtn.addEventListener("click", function(){
+    let arrTask = AddTask.value;
+    if(!arrTask){
+        alert("Please fill out the Task");
+        return;
     }
     else{
-        add[x] = newTask;
-        mood = 'ADD';
-        AddBtn.innerHTML = "Add";
-        addTask.value = '';
+        add.push(arrTask);
     }
-    localStorage.setItem('Task', JSON.stringify(add));
-    // console.log(add);
-    task();
-    // clear();
-}
-function task(){      
-    let show = '';
-    for(let i = 0; i < add.length; i++){
-        show += `
-        <div id = "task">
-            <h3> ${add[i]} </h3> 
-            <i class="fa"> &#xf00c; </i>
-            <button id="EdtieBtn" onclick = 'edite(${i})'> Edite</button> 
-            <button id="DeleteBtn" onclick = 'deleteTask(${i})'> Delete</button> 
-            <button id="DoneBtn" onclick = 'done(${i})'> Done</button>
-        </div>
-        `;
+    AddTask.value = "";
+    display();
+});
+function display(){
+    let task;
+    let head;
+    let text;
+    let btnEdite;
+    let textedite;
+    let btnDelete;
+    let textdelete;
+    let btnDone;
+    let textdone;
+    add.filter((item, index) => {
+        task = document.createElement("div");
+        task.classList.add("task");
+        head = document.createElement("h3");
+        head.classList.add("head");
+        task.appendChild(head);
+        text = document.createTextNode(`${add[index]}`);
+        head.appendChild(text);
+        btnEdite = document.createElement("button");
+        btnEdite.classList.add("btnEdite");
+        textedite = document.createTextNode("Edite");
+        btnEdite.appendChild(textedite);
+        task.appendChild(btnEdite);
+        btnDelete = document.createElement("button");
+        btnDelete.classList.add("btnDelete");
+        textdelete =document.createTextNode("Delete");
+        btnDelete.appendChild(textdelete);
+        task.appendChild(btnDelete);
+        btnDone = document.createElement("button");
+        btnDone.classList.add("btnDone");
+        textdone = document.createTextNode("done");
+        btnDone.appendChild(textdone);
+        task.appendChild(btnDone);
+        btnEdite.addEventListener("click", function(){
+            EditeTask(`${index}`);
+        })
+        btnDone.addEventListener("click", function(){
+            head.style.color = "rgb(83, 223, 83)";
+        })
+    });
+    NewTask.appendChild(task);
+    //DELETE TASK
+    btnDelete.addEventListener("click", function(){
+        deleteTask();
+    })
+    function deleteTask(){
+        while(task.hasChildNodes()){
+            task.removeChild(task.firstChild);
+        }
+        task.classList.remove("task");
     }
-    document.getElementById('tasks').innerHTML = show;
-}
-task();
-function deleteTask(i){
-    add.splice(i, 1);
-    localStorage.Task = JSON.stringify(add);
-    task();
-}
-function edite(i){
-    addTask.value = add[i];
-    AddBtn.innerHTML = "Edite";
-    mood = 'EDITE';
-    x = i;
-}
-function done(i){
-    let check = document.querySelectorAll('i')[i];
-    if(display == 0){
-        check.style.visibility = 'visible';
-        display = 1;
+    //EDITE TASK
+    function EditeTask(index){
+        document.querySelector(".edite").style.display = "block";
+        document.querySelector(".EditeInput").value = add[index];
+        editebtn.addEventListener("click", function(){
+            document.querySelector(".edite").style.display = "none";
+            add[index] = document.querySelector(".EditeInput").value;
+            let a = task.children[0];
+            let s = document.createTextNode(add[index]);
+            a.replaceChild(s, a.childNodes[0]);
+        });
     }
-    else{
-        check.style.visibility= 'hidden';
-        display = 0;
-    }
-}
+};
