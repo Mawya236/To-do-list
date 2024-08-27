5433f1ed0b89caea62911b3b59def57dc09442b7
Mawya236 <mayaellaithy99@gmail.com>
2024-08-27 22:20:54 +0300

    add

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let edite = document.getElementById("edite");
 let EditeInput = document.getElementById("EditeInput");
 let updatebtn = document.getElementById("updatebtn");
 let add = [];
+if(localStorage.getItem('Tasks')){
+    add = JSON.parse(localStorage.getItem("Tasks"))
+}
+getData();
 //ADD TASK
 AddBtn.addEventListener('click', function(){
     let arrTask = AddTask.value;
@@ -14,9 +18,10 @@ AddBtn.addEventListener('click', function(){
     else{
         add.push(arrTask);
         AddTask.value = "";
-        display();
-        localStorage.setItem("Tasks", JSON.stringify(arrTask));
     }
+    // localStorage.setItem("Tasks", JSON.stringify(add));
+    saveData(add);
+    display();
 });
 let editebtn;
 function display(){
@@ -30,8 +35,18 @@ function display(){
             </li>`
     }
     NewTask.innerHTML = show;
-    localStorage.getItem("Tasks");
 };
+// display();
+function saveData(add){
+        window.localStorage.setItem("Tasks", JSON.stringify(add));
+}
+function getData(){
+    let store = window.localStorage.getItem("Tasks");
+    if(store){
+        let tasks = JSON.parse(store);
+        display(tasks);
+    }
+}
 //EDITE TASK
 function EditeTask(i){
     edite.style.display = 'block';
@@ -42,11 +57,13 @@ function EditeTask(i){
 function UpdateTask(i){
     add[i] = EditeInput.value;
     edite.style.display = 'none';
+    saveData(add)
     display();
 }
 //DELETE TASK
 function DeleteTask(i){
     add.splice(i, 1);
+    saveData(add)
     display();
 }
 //DONE TASK
